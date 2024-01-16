6cc2a67e12b1e32dc287b7c7c76b5b910603c225
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-16 19:38:52 +0530

    Add files via upload

---
+++ Printdatatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num;
+    num=2;
+    char c;
+    c='A';
+    double numb2;
+    numb2=1233.34;
+    string s = "Rutuja";
+    cout<<num <<endl;
+    cout<<c<<endl;
+    cout<<numb2<<endl;
+    cout<<s;
+
+
+
+
+}
