f29acc063a0847282872ad60fab4d5dbce5a1e28
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-17 21:10:44 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x=50,y=20,z=90;
+  cout<<"addition="<<x+y+z<<endl;
+  cout<<"multiplication="<<x*y*z<<endl;
+  cout<<"subtraction="<<z-y-x;
+}
