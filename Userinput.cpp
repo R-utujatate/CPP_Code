107cfea7cc8db6ffe8b39e50bf4d196230bb1d88
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-17 21:09:58 +0530

    Add files via upload

---
+++ Userinput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int x,y;
+    cout<<"enter the value of first integer=";
+    cin>>x;
+    cout<<"enter the second integer=";
+    cin>>y;
+    cout<<"addition="<<x+y<<endl;
+
+}
