20ce6f851d1f42ed769eceb57be7b38bf49907ee
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-23 19:13:34 +0530

    Add files via upload

---
+++ string2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+    cout<<max(10,70)<<endl;
+    cout<<min(5,3)<<endl;
+    cout<<sqrt(81)<<endl;
+    cout<<round(2.6)<<endl;
+    return 0;
+
+
+}
