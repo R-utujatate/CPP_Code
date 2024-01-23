f706da2c6e1b2fa505db18b2e57d95a5f42cf755
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-23 19:12:23 +0530

    Add files via upload

---
+++ ifelse1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int myage = 23;
+    int votingAge = 18;
+
+    if(myage >= votingAge)
+    {
+        cout<<"Old enough to vote!";
+
+    }
+    else{
+        cout<<"not enough to vote.";
+    }
+
+}
