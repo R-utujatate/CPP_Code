bcfe24ef569cab83c5a7989af52810641e48c84c
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-01-29 22:38:01 +0530

    Add files via upload

---
+++ swapnumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int a,b,c;
+    cout<<"enter two numbers:";
+    cin>>a>>b;
+
+    c=a;
+    a=b;
+    b=c;
+    cout<<"a:"<<a<<" "<<"b:"<<b;
+
+
+
+}
